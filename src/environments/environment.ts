c668d0ec3fdfeb22b217c4816dd33cde5479212c
Jean-François Lagarde <jflagarde@jflagarde.com>
2017-05-23 13:32:26 -0400

    Add a placeholder for Firebase credentials

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: '<API_KEY>',
+    authDomain: '<PROJECT_ID>.firebaseapp.com',
+    databaseURL: 'https://<DATABASE_NAME>.firebaseio.com',
+    projectId: '<PROJECT_ID>',
+    storageBucket: '<BUCKET>.appspot.com',
+    messagingSenderId: '<SENDER_ID>',
+  }
 };
