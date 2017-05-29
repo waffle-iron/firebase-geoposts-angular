45944e6d1f0b3d2ee1c0e68709a273f19ca9820e
Jean-François Lagarde <jflagarde@jflagarde.com>
2017-05-28 20:49:51 -0400

    Login page and Auth

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
-@Component({
+import { AuthService } from 'app/core/auth.service';
+import * as firebase from 'firebase/app';
+
+@Component( {
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
-})
-export class AppComponent {
+} )
+export class AppComponent implements OnInit {
   title = 'app works!';
+  user: firebase.User;
+
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) { }
+
+  ngOnInit() {
+    this.authService.user$.subscribe(
+      ( res ) => {
+        this.user = res;
+        if ( res ) {
+          this.router.navigate( [''] );
+        }
+      }
+    );
+  }
+  logout(): firebase.Promise<any> {
+    return this.authService.logout()
+      .then(( data ) => { console.log( 'logged out' ); } )
+      .catch(( err ) => { console.log( err ); } );
+  }
 }
